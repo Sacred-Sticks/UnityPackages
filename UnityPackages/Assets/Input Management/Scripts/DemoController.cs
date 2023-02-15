30b6ef87d49fc1d79ab192d001dab8bea41b345b
Lucas Ackman <AckmanLD@gmail.com>
2023-02-15 00:47:37 -0600

    Update DemoController.cs

--- UnityPackages/Assets/Input Management/Scripts/DemoController.cs
+++ UnityPackages/Assets/Input Management/Scripts/DemoController.cs
@@ -20,8 +20,40 @@ public class DemoController : MonoBehaviour
     private void Start()
     {
         // Subscribe to events in inputManager sciptable object
+        inputManager.OnInputEvent += RecievePlayerInput;
         inputManager.OnInputPressedEvent += RecievePlayerInputPressed;
         inputManager.OnInputReleasedEvent += RecievePlayerInputReleased;
+    }
+
+    private void RecievePlayerInput(object sender, PlayerInputArguments e)
+    {
+        // Makeshift Switch Statement because SO's cannot be declared as const, therefore cannot be a case
+        if (e.InputType == movementEnums.Up)
+        {
+            Debug.Log("Up Value Changed");
+            goto InputReceived;
+        }
+        if (e.InputType == movementEnums.Down)
+        {
+            Debug.Log("Down Value Changed");
+            goto InputReceived;
+        }
+        if (e.InputType == movementEnums.Left)
+        {
+            Debug.Log("Left Value Changed");
+            goto InputReceived;
+        }
+        if (e.InputType == movementEnums.Right)
+        {
+            Debug.Log("Right Input Changed");
+            goto InputReceived;
+        }
+        // Default Case Goes Here
+        Debug.Log("Default Case Input");
+        return;
+    InputReceived:
+        // Anything After Switch Statement still needed after modifying values goes here
+        Debug.Log("Input Modified");
     }
 
     private void RecievePlayerInputPressed(object sender, PlayerInputArguments e)
@@ -29,25 +61,26 @@ public class DemoController : MonoBehaviour
         // Makeshift Switch Statement because SO's cannot be declared as const, therefore cannot be a case
         if (e.InputType == movementEnums.Up)
         {
-            inputs.y = inputs.y < 0 ? 0 : 1;
+            inputs.y = inputs.y == 0 ? 1 : 0;
             goto InputReceived;
         }
         if (e.InputType == movementEnums.Down)
         {
-            inputs.y = inputs.y > 0 ? 0 : -1;
+            inputs.y = inputs.y == 0 ? -1 : 0;
             goto InputReceived;
         }
         if (e.InputType == movementEnums.Left)
         {
-            inputs.x = inputs.x > 0 ? 0 : -1;
+            inputs.x = inputs.x == 0 ? -1 : 0;
             goto InputReceived;
         }
         if (e.InputType == movementEnums.Right)
         {
-            inputs.x = inputs.x < 0 ? 0 : 1;
+            inputs.x = inputs.x == 0 ? 1 : 0;
             goto InputReceived;
         }
         // Default Case Goes Here
+        Debug.Log("Default Case Input Pressed");
         return;
         InputReceived:
         // Anything After Switch Statement still needed after modifying values goes here
@@ -60,25 +93,26 @@ public class DemoController : MonoBehaviour
         // Makeshift Switch Statement because SO's cannot be declared as const, therefore cannot be a case
         if (e.InputType == movementEnums.Up)
         {
-            inputs.y = inputs.y > 0 ? 0 : 1;
+            inputs.y = inputs.y == 0 ? -1 : 0;
             goto InputReceived;
         }
         if (e.InputType == movementEnums.Down)
         {
-            inputs.y = inputs.y < 0 ? 0 : 1;
+            inputs.y = inputs.y == 0 ? 1 : 0;
             goto InputReceived;
         }
         if (e.InputType == movementEnums.Left)
         {
-            inputs.x = inputs.x < 0 ? 0 : 1;
+            inputs.x = inputs.x == 0 ? 1 : 0;
             goto InputReceived;
         }
         if (e.InputType == movementEnums.Right)
         {
-            inputs.x = inputs.x > 0 ? 0 : 1;
+            inputs.x = inputs.x == 0 ? -1 : 0;
             goto InputReceived;
         }
         // Default Case Goes Here
+        Debug.Log("Default Case Input Released");
         return;
         InputReceived:
         // Anything After Switch Statement still needed after modifying values goes here
