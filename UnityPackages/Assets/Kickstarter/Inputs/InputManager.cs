87a5b3af3b64c505fba65fe2afa669971a165f8a
Lucas Ackman <AckmanLD@gmail.com>
2023-05-17 02:26:00 -0500

    Set range from 1-4

--- UnityPackages/Assets/Kickstarter/Inputs/InputManager.cs
+++ UnityPackages/Assets/Kickstarter/Inputs/InputManager.cs
@@ -7,7 +7,7 @@ namespace Kickstarter.Inputs
     [CreateAssetMenu(fileName = "Input Manager", menuName = "Inputs/Input Manager")]
     public class InputManager : ScriptableObject
     {
-        [Range(0, 4)]
+        [Range(1, 4)]
         [SerializeField] private int maxPlayerCount;
         [Space(20)]
         [SerializeField] private InputAssetObject[] inputObjects;
